63fe0cab6d8dfd8cbcd76c2f995305c093b9b723
Jesús Otero <jesusoterogomez@gmail.com>
2015-02-26 01:17:10 -0430

    Added nerds website

---
+++ nerds/js/design.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function(){
+
+
+/*Get Colors from HTML Tags*/
+$(".color").each(function( index ) {
+	var Hex = $(this).find('#hex').html();
+	$(this).find('#picker').css("background",Hex);
+ 	
+	});
+
+$(".link").each(function( index ) {
+	var Hex = $(this).find('#hex').html();
+	$(this).find('.a').css("color",Hex);
+	});
+
+/*Create Jquery UI Spinner for USD*/
+$('.spinner').spinner({
+    min: 0,
+    max: 2500,
+    step: 25,
+    numberFormat: "C" /*Currency Format*/
+});
+
+/*Dropdown List*/
+	$('.ui-dropdown #selector').click(function() {
+		event.preventDefault();
+        $('.ui-dropdown .list').toggleClass("active");
+    });
+
+	$('.ui-dropdown .list li').click(function() {
+		//alert('Zing');
+		$('.ui-dropdown .list li').removeClass("selected");
+		$(this).toggleClass("selected");
+	    $('.ui-dropdown .list').toggleClass("active");
+		$('.ui-dropdown #box').attr("data-selected",$(this).find("a").attr('data-value'));
+		$('.ui-dropdown #box').html($(this).find("a").html());  
+	});
+
+	$('.ui-dropdown').focusout(function() {
+	   $('.ui-dropdown .list').removeClass("active");
+	});
+
+});
---
+++ nerds/mail.php
@@ -0,0 +1,33 @@
+﻿<?php
+$mail = $_POST['mail'];
+
+$name = $_POST['name'];
+
+$text = $_POST['text'];
+
+$to = 'email@dominio.com';
+
+$name = htmlspecialchars($name, ENT_QUOTES);
+$text = htmlspecialchars($text, ENT_QUOTES);
+$name = stripslashes($name);
+$text = stripslashes($text);
+$message = '<html><body>';
+$message .= '<h3>Nuevo mensaje de '.$name.' <a href="mailto:'.$mail.'">: '.$mail.'</a> </h3>';
+$message .= '<p>'.$text.'</p>';
+$message .= '</body></html>';
+$subject = 'Formulario:'.$mail; 
+$headers = 'From: '.$name.' <email@dominio.com>' . "\r\n" . 
+    'Reply-To:'.$mail.''. "\r\n" . 
+    'X-Mailer: PHP/' . phpversion(); 
+$headers .= "Reply-To: email@dominio.com" . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+$message = stripslashes($message);
+if(mail($to,$subject,$message,$headers)){
+echo "Gracias por enviarme un mensaje";
+}
+else{
+echo "No pudimos enviar tu mensaje. Intenta más tarde";
+}
+?>
